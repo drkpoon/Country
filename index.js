7be767491914689e108d79eca22448bc5a80097e
drkpoon <drkpoon@hgcbroadband.com>
2017-06-28 22:59:11 +0800

    Change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ agenda.on('fail', function(job){
   job.remove();
 }); */
 
-mongoose.connect('mongodb://172.31.60.3/countries');
+mongoose.connect('mongodb://172.30.243.251/countries');
 var conn = mongoose.connection;
 Grid.mongo = mongoose.mongo;
 var gfs = Grid(conn.db);
