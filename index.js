78ef69d236220f253f4c0bccd97df7c10a0f8ab5
drkpoon <drkpoon@hgcbroadband.com>
2018-03-15 21:04:55 +0800

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var Agenda = require('agenda')
 var NumberParser = require('numberparser');
 
 if (process.env.MONGODB_USER){
-  var rootUrl = 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_ADMIN_PASSWORD + '@10.128.106.62:27017'
+  var rootUrl = 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_ADMIN_PASSWORD + '@10.129.20.47:27017'
 }else{
   var rootUrl = 'mongodb://' + 'derek' + ':' + 'derek' + '@localhost:27017'
 }
